a30622b87c2293f9b0e0d11f6855588bc511aca3
Bongani Siamtinta <bsiamtinta@gmail.com>
2022-06-08 09:35:58 -0700

    98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints two numbers added together
+ * @i: print int i
+ * @k: print int k
+ * Return: always 0 (success)
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
+
