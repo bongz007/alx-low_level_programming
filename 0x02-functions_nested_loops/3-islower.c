c6d24eb64e5617f6c33bc1f02c499c9cc4a594a8
Bongani Siamtinta <bsiamtinta@gmail.com>
2022-06-08 08:02:38 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - short description
+ * @c: contains value to be compared
+ * Return: Always 0 (success)
+ */
+int _islower(int c)
+{
+	
+	if (c > 'a' &&  c < 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return(0)
+	}
+
+}
