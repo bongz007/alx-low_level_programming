6106d6352ec7778219c9b161136e6c2f10f5d439
Bongani Siamtinta <bsiamtinta@gmail.com>
2022-06-13 17:29:09 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an integer value to 96
+ * @n: Apointer to an int that will be updated
+ *
+ * Return: void that means its correct
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
+
